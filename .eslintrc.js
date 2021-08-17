59e3536c54ab9550df2cdc830e20078921659da5
Michell Algarra <michell.algarra@gmail.com>
2021-08-16 23:54:05 -0300

    feat: created the project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 8,
+      sourceType: 'module',
+    },
+    plugins: ['import-helpers'],
+    extends: ['prettier'],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          newlinesBetween: 'always',
+          groups: [
+            '/express/',
+            '/socket/',
+            '/utils/',
+            ['index'],
+          ],
+          alphabetize: {
+            order: 'asc',
+            ignoreCase: true,
+          },
+        },
+      ],
+    },
+  }
