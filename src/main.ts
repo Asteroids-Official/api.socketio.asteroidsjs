d94362d34dcca75b397e79b0e73411ceba6e713a
Michell Algarra <michell.algarra@gmail.com>
2021-08-25 03:01:19 -0300

    feat(main.ts): added some socket event listeners to configure and generate the player

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Server, Socket } from 'socket.io'
 
 import { config } from 'dotenv'
 import { createServer } from 'http'
+import { Player, PlayerInfo } from './interfaces/player.interface'
 
 config()
 
@@ -12,8 +13,68 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server().listen(httpServer)
 
+const rooms: {
+  [key: string]: { [id: string]: Player }
+} = {}
+
 io.on('connection', (socket: Socket) => {
   console.log('Connected: ' + socket.id)
+
+  /**
+   * Joins the multiplayer on-line game.
+   *
+   * @param playerInfo - The player information.
+   * @param callback - A function to be called when the game is joined.
+   */
+  function joinGame(
+    id = '',
+    playerInfo: PlayerInfo,
+    callback: (player: Player) => void,
+  ): void {
+    let playerId: string
+
+    if (Object.keys(rooms).length === 0) {
+      rooms['room1'] = {}
+      playerId = '1'
+    } else if (!id) {
+      const ids = Object.keys(rooms['room1'])
+        .map((id) => +id)
+        .sort((id1, id2) => id1 - id2)
+      playerId = ids[ids.length - 1] + 1 + ''
+    }
+
+    if (id) {
+      playerId = id
+    }
+
+    const player: Player = {
+      id: playerId,
+      ...playerInfo,
+      health: 100,
+      isShooting: false,
+    }
+
+    rooms['room1'][playerId] = player
+
+    socket.join('room1')
+
+    callback(player)
+    io.to('room1').emit('join-player', player)
+  }
+  socket.on('join-game', joinGame)
+
+  /**
+   * Updates a player into a specific room.
+   *
+   * @param room - The room that will receive the update.
+   * @param player - The player that will receive the update.
+   */
+  function updatePlayer(room: string, player: Player): void {
+    rooms[room][player.id] = player
+
+    io.to(room).emit('update-game', rooms[room])
+  }
+  socket.on('update-player', updatePlayer)
 })
 
 io.on('disconnect', (reason: string) => {
